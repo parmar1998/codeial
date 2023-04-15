4fbcbcf4dfb2edca23fd33d7e625e5f6456b3375
parmar1998 <128887283+parmar1998@users.noreply.github.com>
2023-04-15 18:35:26 +0530

    Added files till EJS setup

--- Routes/index.js
+++ Routes/index.js
@@ -2,5 +2,7 @@ const express=require('express');
 const router=express.Router();
 console.log("router loaded");
 const homeController=require('../Controllers/home_controller')
-router.get('/',homeController.home)
+router.get('/',homeController.home);
+//here we are telling  root of routes we will have to tell it list 
+router.use('/users',require('./users'))
 module.exports=router;
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app=express();
 const port =9000;
 //use express router
 app.use('/',require('./Routes'));
+app.set('view engine','ejs');
+app.set('views','./Views')
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server on ${port}`);
